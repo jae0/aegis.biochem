d45f7de4df52b0b7da8579b5e78a12020a5f2f25
Jae S Choi <choi.jae.seok@gmail.com>
2019-06-04 10:43:46 -0300

    misc

---
+++ R/biochem_parameters.R
@@ -0,0 +1,62 @@
+
+
+biochem_parameters = function( p=NULL, project.name=NULL, project.mode="default", ... ) {
+
+  # ---------------------
+  # deal with additional passed parameters
+  if ( is.null(p) ) p=list()
+  p_add = list(...)
+  if (length(p_add) > 0 ) p = c(p, p_add)
+  i = which(duplicated(names(p), fromLast = TRUE ))
+  if ( length(i) > 0 ) p = p[-i] # give any passed parameters a higher priority, overwriting pre-existing variable
+
+
+  # ---------------------
+  # create/update library list
+  p$libs = c( p$libs, RLibrary ( "colorspace",  "fields", "geosphere", "lubridate",  "lattice",
+    "maps", "mapdata", "maptools", "parallel",  "rgdal", "rgeos",  "sp", "splancs", "GADMTools" ) )
+  p$libs = c( p$libs, project.library ( "aegis", "aegis.biochem" ) )
+
+  p$project.name = ifelse ( !is.null(project.name), project.name, "biochem" )
+
+  if ( !exists("data_root", p) ) p$data_root = project.datadirectory( "aegis", p$project.name )
+  if ( !exists("datadir", p) )   p$datadir  = file.path( p$data_root, "data" )
+  if ( !exists("modeldir", p) )  p$modeldir = file.path( p$data_root, "modelled" )
+
+  if ( !file.exists(p$datadir) ) dir.create( p$datadir, showWarnings=F, recursive=T )
+  if ( !file.exists(p$modeldir) ) dir.create( p$modeldir, showWarnings=F, recursive=T )
+
+  if (!exists("spatial.domain", p) ) p$spatial.domain = "SSE"
+  if (!exists("spatial.domain.subareas", p)) p$spatial.domain.subareas = c( "snowcrab", "SSE.mpa" )
+  p = spatial_parameters( p=p)
+
+  # define focal years
+  if (!exists( "yrs", p)) p$yrs = 1950:lubridate::year(lubridate::now())  # default
+
+
+  if (project.mode=="default") {
+    return(p)
+  }
+
+
+  if (project.mode=="stmv") {
+    p$libs = c( p$libs, project.library ( "stmv" ) )
+    p$DATA = 'biochem.db( p=p, DS="stmv_inputs" )'
+    p$varstomodel = c( )
+    if (!exists("variables", p)) p$variables = list()
+    if (!exists("LOCS", p$variables)) p$variables$LOCS=c("plon", "plat")
+    if (!exists("TIME", p$variables)) p$variables$TIME="tiyr"
+    p = aegis_parameters(p=p, DS="stmv_spatiotemporal_model", stmv_dimensionality="space-year" )
+    return(p)
+  }
+
+
+
+  if (project.mode=="carstm") {
+    p$libs = c( p$libs, project.library ( "carstm" ) )
+
+    return(p)
+  }
+
+
+}
