534b9dede54e89de51deda39550a28e9e439b8f2
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-23 13:58:26 -0300

    reorg param calls

--- R/biochem_parameters.R
+++ R/biochem_parameters.R
@@ -55,7 +55,7 @@ biochem_parameters = function( p=NULL, project_name=NULL, project_class="default
 
   if (project_class=="carstm") {
     p$libs = c( p$libs, project.library ( "carstm" ) )
-    p = aegis_parameters(p=p, DS="carstm") # generics
+    p = carstm_parameters(p=p, DS="basic") # generics
     return(p)
   }
 
